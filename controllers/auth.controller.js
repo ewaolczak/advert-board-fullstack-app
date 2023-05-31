44425b56121560d95e0c48ae63d0382e41c8e03d
Ewa Olczak <ewa.olczak@gmail.com>
2023-05-31 08:38:45 +0200

    Add phone to register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,29 +3,30 @@ const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   try {
-    const { login, password } = req.body;
+    const { login, password, phone } = req.body;
     if (
       login &&
       typeof login === 'string' &&
       password &&
-      typeof password === 'string'
+      typeof password === 'string' &&
+      phone &&
+      typeof phone === 'string' //dlaczego tutaj jest string a w modelu number??
     ) {
       const userWithLogin = await User.findOne({ login });
       if (userWithLogin) {
-        return res
-          .status(409)
-          .send({ message: 'User with this login already exists' });
+        return res.status(409).send({ message: 'User with this login already exists' });
       }
 
       const user = await User.create({
         login,
-        password: await bcrypt.hash(password, 10)
+        password: await bcrypt.hash(password, 10),
+        phone
       });
-      res.status(201).send({ message: 'User created ' + user.login });
+      res.status(201).send({ message: `Created user ${user.login}` });
     } else {
       res.status(400).send({ message: 'Bad request' });
     }
-  } catch {
+  } catch (err) {
     res.status(500).send({ message: err });
   }
   // res.send('register');
