9c8cd7e1a772b6494132e50cd8f443ba0358c188
Ewa Olczak <ewa.olczak@gmail.com>
2023-06-07 20:51:28 +0200

    Fix error with header

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 
 // components
-import Header from './components/views/Header/Header';
+import Header from './components/pages/Header/Header';
 import Home from './components/pages/Home/Home';
 import Footer from './components/pages/Footer/Footer';
 import { Route, Routes } from 'react-router-dom';
